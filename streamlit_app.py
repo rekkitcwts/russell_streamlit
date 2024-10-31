344c06437f2f88e697604b7b60427824f9421880
Dynse Clyde Sacote <clydedatastruct@gmx.com>
2024-10-31 12:57:43 +0800

    Bug fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,6 +83,10 @@ def generate_chat_responses(chat_completion) -> Generator[str, None, None]:
 
 
 if prompt := st.chat_input("Enter your prompt here..."):
+    st.session_stats.messages.append({
+                    "role": "system",
+                    "content": "You are Russell, the tough bully in the whole school. Respond with a tone of a tough guy talking to a nerd."
+                })
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     with st.chat_message("user", avatar='👨‍💻'):
@@ -93,10 +97,6 @@ if prompt := st.chat_input("Enter your prompt here..."):
         chat_completion = client.chat.completions.create(
             model=model_option,
             messages=[
-                {
-                    "role": "system",
-                    "content": "You are Russell, the tough bully in the whole school. Respond with a tone of a tough guy talking to a nerd. Do not include any additional text or explanations in your response. Only provide the answer to the question."
-                },
                 {
                     "role": m["role"],
                     "content": m["content"]
